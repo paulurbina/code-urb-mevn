95210ad5a3cb5a7c12f946e9965adb3ecfda290e
tonypera <paulurbian@hotmail.com>
2018-07-27 20:43:27 -0500

    banner

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <Navigation_menu></Navigation_menu>
+        <Banner_principal></Banner_principal>
     </div>
 </template>
 
 <script>
 import Navigation from './components/Navigation.vue';
+import Banner from './components/Banner.vue';
 
 export default {
     components: {
-        Navigation_menu: Navigation
+        Navigation_menu: Navigation,
+        Banner_principal: Banner
     },
 }
 </script>
---
+++ src/app/components/Banner.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+         <header id="header" class="">
+            <div class="container-fluid">
+                <div class="row position-relative text-center">
+                    <img id="style-banner" class="img-fluid" src="/assets/banner-principal.jpg">
+                    <p class="texto-banner position-absolute">
+                        Diseñamos tu sitio web<br>
+                        con un enfoque diferente
+                    </p>
+                </div>
+            </div>
+         </header>
+    </div>
+</template>
+
+<style>
+#style-banner {
+	background-position: center center;
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	background-size: cover; 
+	background-color: #898989;
+	width: 100%;
+	height: 610px;
+}
+
+@media all and (max-width: 500px) {
+    #style-banner {
+        width: 100%;
+        height: 400px;
+        overflow: hidden;
+    }
+    #header .texto-banner {
+        font-size: 20px;
+    }
+}
+#header .texto-banner {
+	font-size: 50px;
+	bottom: 90px;
+	right: 90px;
+	color: #fff;
+}
+</style>
