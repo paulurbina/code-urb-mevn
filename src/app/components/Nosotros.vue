b2b789e13f66d8ff48932e5dd9c96a2c788e0029
tonypera <paulurbian@hotmail.com>
2018-07-30 00:29:21 -0500

    update nosotros

--- src/app/components/Nosotros.vue
+++ src/app/components/Nosotros.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
         <section id="nosotros">
-            <div class="container-fluid">
+            <div class="container-fluid mt-5">
                 <div class="row">
                     <div class="col-md-6 col-xs-6">
                         <div class="nosotros-content-left">
-                            <img class="img-fluid" src="/assets/nosotros.jpg" width="100%;" alt="">
+                            <img class="img-fluid" src="/assets/nosotros-section.png" width="100%;" alt="">
                         </div>
                     </div>
                     <div class="col-md-6 col-xs-6">
                         <div class="nosotros-content-right middle-xs">
-                                <h2 class="nosotros-titulo">EN SYSTEMS URB</h2>
-                                <P class="text-justify">
-                                Te damos el mejor servicio web de calidad para aumentar la rentabilidad y posicionamiento
+                                <h2 class="nosotros-titulo mt-5">EN SYSTEMS URB</h2>
+                                <P class="nosotros-description mt-5">
+                                Te damos el mejor servicio web <br> de calidad para aumentar la rentabilidad <br> y posicionamiento
                             </P>
                         </div>
                     </div>
@@ -27,10 +27,14 @@
        font-size: 30px;
        font-family: MyriadPro-Regular;
    }
+   
 }
 
 @media screen and (max-width: 750px) and (min-width: 401px) {
-
+    .nosotros-titulo {
+        font-size: 15px;
+        text-align: center;
+    }
 }
 @media screen and (max-width: 400px) {
     
