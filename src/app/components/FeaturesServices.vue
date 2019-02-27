76526539d524aa383f30e93828ae45e766824884
tonypera <paulurbian@hotmail.com>
2019-02-27 01:16:03 -0500

    update modal

--- src/app/components/FeaturesServices.vue
+++ src/app/components/FeaturesServices.vue
@@ -16,19 +16,24 @@
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                    <!--MODAL HEADER-->
-                 <div class="modal-header">
-                   <h4 class="modal-title">
-                     {{featur.title}}
+                 <div class="modal-header" style="background-color: black;">
+                   <i :class="featur.icons"></i>  
+                   <h4 class="modal-title" style="padding-left: 10px;">
+                     <a href="#">
+                      {{featur.title}}
+                     </a>
                    </h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <button type="button" class="close" data-dismiss="modal" style="color:#18d26e;">
+                      <div style="font-size: 35px;">
+                        &times;
+                      </div>
+                    </button>
                  </div>
                  <!--MODAL BODY-->
-                 <div class="modal-body">
-                   {{featur.body}}
-                 </div>
-                 <!--MODAL-->
+                 <div class="modal-body text-center text-justify" v-html="featur.body"></div>
+                 <!--MODAL FOOTER-->
                  <div class="modal-footer">
-                   <button  type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                   <button  type="button" class="btn btn-secondary entendido" data-dismiss="modal">Entendido!</button>
                  </div>
                 </div>
               </div>
@@ -53,21 +58,35 @@ export default {
           id: 'modalPresencia',
           icons: 'ion-ios-bookmarks-outline',
           title: 'Presencia Digital',
-          body: 'Modal Body..'
+          body: `
+            &#8226; Página Web o landing Page desde 0 <br>
+            &#8226; Google My Business <br>
+            &#8226; Creación o renovación de presentación de redes sociales (Chat Boots) <br>
+          `
         },
         {
           data_target: '#modalTrafico',
           id: 'modalTrafico',
           icons: 'ion-ios-stopwatch-outline',
           title: 'Trafico de usuarios',
-          body: 'Modal body..'
+          body: `
+            &#8226; Página Web, landing Page o E commers desde 0 <br>
+            &#8226; Plan de redes sociales ( Chat Boots, formulario) <br>
+            &#8226; Plan de Google Adwords ( publicidad y posicionamiento ) <br>
+            &#8226; Asesoría y mantenimiento web (mensual)
+          `
         },
         {
           data_target: '#modalGeneracion',
           id: 'modalGeneracion',          
           icons: 'ion-ios-heart-outline',
           title: 'Generacion de Clientes Potenciales',
-          body: 'Modal body...'
+          body: `
+              &#8226; Página Web, landing Page o E commers desde 0. <br>
+              &#8226; Trafico de usuarios en redes sociales (FB Ads, Chat Boots y formulario) <br>
+              &#8226; CEO (posicionamiento web), Google Adwords. <br>
+              &#8226; Asesoría y mantenimiento web (mensual)
+          `
         }
       ]
     }
@@ -75,13 +94,17 @@ export default {
 }
 </script>
 
-
 <style scoped>
+
+button.entendido {
+  background-color: #000;
+}
+
 #featured-services.box {
      height: 340px;
  }
 
-#featured-services {
+#featured-services{
   background: #000;
 }
 
@@ -109,13 +132,6 @@ export default {
 
 #featured-services h4 a:hover {
   color: #18d26e;
-}
-
-#featured-services p {
-  font-size: 14px;
-  line-height: 24px;
-  color: #fff;
-  margin-bottom: 0;
 }
 
 </style>
